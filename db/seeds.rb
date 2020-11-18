9c5886d908c6e2693d2975f5cc0421886cf08044
nicolas blum <nblumferracci@gmail.com>
2020-11-18 14:38:19 +0100

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'creating 5 restaurants'
+
+macdo = Restaurant.create(
+  name: "macdo",
+  address: "Nantes",
+  category: "fastfood"
+)
+francois = Restaurant.create(
+  name: "chez francçois",
+  address: "Nantes",
+  category: "french"
+)
+michel = Restaurant.create(
+  name: "chez michel",
+  address: "Paris",
+  category: "chinese"
+)
+
+quick = Restaurant.create(
+  name: "Quick",
+  address: "Lyon",
+  category: "fastfood"
+)
+
+les_bouteilles = Restaurant.create(
+  name: "Les bouteilles",
+  address: "Nantes",
+  category: "french"
+)
+puts 'finish'
